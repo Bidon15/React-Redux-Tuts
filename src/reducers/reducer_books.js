77c9d93947719608321c14c2be9fc5b80699e9e1
Nhu Viet Nguyen <nhuvietnguyen@Nhus-MacBook-Pro.local>
2017-11-03 07:45:21 +0200

    Delete uneccesary modules

--- src/reducers/reducer_books.js
+++
@@ -1,9 +0,0 @@
-export default function () {
-	return [
-		{title: 'Javascript: The Good Part', pages: 101},
-		{title: 'Apple Designer Book', pages: 300},
-		{title: 'Eric Burn: People who play games, games which people play', pages: 200},
-		{title: 'John Grey: Man from Mars, Woman from Venus', pages: 450},
-		{title: 'Why we love to love? A handguide to a happy and fullfilled life', pages: 600}
-	];
-}
